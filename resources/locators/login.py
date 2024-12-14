3ae75d13fceaaa3f7eac0d6cbc53e040f85496ac
qa8990 <76061897+qa8990@users.noreply.github.com>
2024-12-14 15:41:50 +0000

    Feat: Config the entire suite framework to test

---
+++ resources/locators/login.py
@@ -0,0 +1,7 @@
+txt_user = '//input[@name="username"'
+txt_password = '//input[@name="password"]'
+button_signin = '//*[@id="login-page"]/div/form/div[3]/button[2]'
+
+# Titles
+WEBSITE_TITLE = 'Ensolvers QA Challenge'
+LOGIN_TITLE = 'Sign in'
---
+++ resources/locators/welcome.py
@@ -0,0 +1,24 @@
+# Welcome page locators
+# Titles
+WELCOME_TITLE = "Welcome to Ensolvers QA Challenge!"
+MAIN_TITLE = " QA Challenge"
+COMPANY_TITLE = ""
+LOGGED_INFO = 'You are logged in as "user".'
+MANAGE_TODO_ITEMS = "  Manage To-Do Items"
+MANAGE_FOLDERS = "  Manage Folders"
+
+<img src="content/images/ensolvers-white.png" alt="Logo">
+# Locators
+main_link = '//a[@href="/"]'
+Click_Link1 = '(//a[@href="/"])[2]'
+Click_Link2  = '//a[@href="#"]'
+to_do_link =   '//a[@href="/to-do-item"]'
+folder_link =   '//a[@href="/folder"]'
+Click_Link3 =   '(//a[@href="#"])[2]'
+account_settings_link =   '//a[@href="/account/settings"]'
+account_logout_link =   '//a[@href="/logout"]'
+button_to_do =   '//button[@class="mr-2 btn btn-info"]'
+button_folder =   '//button[@class="mr-2 btn btn-info"])[2]'
+
+# Images
+company_image_src = 'src="content/images/ensolvers-white.png"'
